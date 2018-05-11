66bdb40fc59566f58f5a9e64659f5d4f17496842
Tobias Leinss <caruso33@web.de>
2018-05-11 16:27:18 +0700

    feat: Implementing Auth

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/App';
+import { BrowserRouter, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import { logger } from 'redux-logger';
+import reducers from './reducers';
+
+import Dashboard from './components/dashboard';
+
+import App from './components/app';
+import Signin from './components/auth/signin';
+import Signout from './components/auth/signout';
+import Signup from './components/auth/signup';
+import Feature from './components/feature';
+import RequireAuth from './components/auth/require_auth';
+import Welcome from './components/welcome';
+import { AUTH_USER } from './actions/types';
+
+// Development only axios helpers!
+import axios from 'axios';
+window.axios = axios;
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk, logger)(
+  createStore
+);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// If we have a token, consider the user to be signed in
+if (token) {
+  // we need to update application state
+  store.dispatch({ type: AUTH_USER });
+}
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={App} />
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        {/* <Route path="/signin" render={() => <div>Hallo</div>} /> */}
+        <Route path="/signin" component={Signin} />
+        <Route path="/signout" component={Signout} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/feature" component={RequireAuth(Feature)} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
